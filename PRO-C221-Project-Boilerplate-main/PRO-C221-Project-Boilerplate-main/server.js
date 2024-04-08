48371c1d3ebd0f0f83d8ea47ee0ad42b9bd4d9e0
Aryan Desai <95379210+Ary-Des@users.noreply.github.com>
2024-04-08 19:49:42 +0100

    Add files via upload

---
+++ PRO-C221-Project-Boilerplate-main/PRO-C221-Project-Boilerplate-main/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.use(express.json())
+
+server.listen(process.env.PORT || 3030);
+var nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: 'smtp.gmail.com',
+    auth: {
+        user:'abc@gmail.com',
+        pass: '',
+    },
+    secure: true,
+})
